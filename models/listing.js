82f3faee0c9896d43b510980a9be1cf70c412b2e
Tanjila <tanjilaakter771746@gmail.com>
2026-08-29 22:14:17 +0600

    update

--- models/listing.js
+++ models/listing.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
+    title: {
+        type:String,
+        required: true,
+
+    },
+
     description: String,
-    image: String,
+    image:{
+        type:String,
+        required: true,
+},
+    
+    String,
     price: Number,
     location: String,
     country: String,
