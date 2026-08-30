65f5b0e04509db909dcba03b4c5ae665b0ce568c
Tanjila <tanjilaakter723874@gmail.com>
2026-08-30 19:59:46 +0600

    error solved

--- app.js
+++ app.js
@@ -38,5 +38,5 @@ app.get("/testListing", async (req, res) => {
 });
 
 app.listen(8080, () => {
-    console.log("server --- is listening to port 8080");
+    console.log( "serveris listening to port 8080");
 });
