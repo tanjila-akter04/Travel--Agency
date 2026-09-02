e62ac369fffcaad3598e4fae5f8e0c82fe0a5cd2
Tanjila <tanjilaakter723874@gmail.com>
2026-09-02 17:10:04 +0600

    update

--- init/data.js
+++ init/data.js
@@ -339,7 +339,8 @@ const sampleListings = [
     title: "Secluded Beach House in Costa Rica",
     description:
       "Escape to a secluded beach house on the Pacific coast of Costa Rica. Surf, relax, and unwind.",
-    image: {
+    
+      image: {
       filename: "listingimage",
       url: "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     },
