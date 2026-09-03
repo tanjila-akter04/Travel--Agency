7831edb1242b79efd1c414ad9292b599711802bb
Tanjila <tanjilaakter723874@gmail.com>
2026-09-03 21:25:48 +0600

    update

--- app.js
+++ app.js
@@ -18,19 +18,27 @@ async function main() {
     await mongoose.connect(MONGO_URL)
 }
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+ app.set("view engine", "ejs");
+ app.set("views", path.join(__dirname, "views"));
+ app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-
-
+//Index Route
 app.get("/listings",async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
 });
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+     let {id} = req.params;
+     const listing = await Listing.findById(id);
+     res.render("listings/show.ejs", { listing });
+});
+
 
 // app.get("/testListing", async (req, res) => {
 //    let sampleListing = new Listing({
