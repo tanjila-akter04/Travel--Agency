8e91f4d9386d969bd8e6e7307e8205a9a49cda3f
Tanjila <tanjilaakter723874@gmail.com>
2026-09-04 15:53:24 +0600

    update

--- app.js
+++ app.js
@@ -32,12 +32,19 @@ app.get("/listings",async (req, res) => {
     res.render("listings/index.ejs", {allListings});
 });
 
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+
 //Show Route
 app.get("/listings/:id", async (req, res) => {
      let {id} = req.params;
      const listing = await Listing.findById(id);
      res.render("listings/show.ejs", { listing });
 });
+
 
 
 // app.get("/testListing", async (req, res) => {
