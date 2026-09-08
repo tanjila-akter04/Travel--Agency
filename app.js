31895d820e70f808cd1c28ed8310182a75830940
Tanjila <tanjilaakter723874@gmail.com>
2026-09-08 20:33:13 +0600

    new

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -21,6 +22,7 @@ async function main() {
  app.set("view engine", "ejs");
  app.set("views", path.join(__dirname, "views"));
  app.use(express.urlencoded({extended: true}));
+ app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
