84e2ec1de88499dca80fdb589e84d09af9f06f9a
Tanjila <tanjilaakter723874@gmail.com>
2026-09-13 02:10:38 +0600

    new

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ async function main() {
  app.use(express.urlencoded({extended: true}));
  app.use(methodOverride("_method"));
  app.engine('ejs', ejsMate);
+ app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
